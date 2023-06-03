a0ff7239052e086434d3a4fac2c2ba3d40acbcb4
Diana <1n2a3r4kakasi@gmail.com>
2023-06-03 21:07:54 +0300

    Мой первый коммит проекта

---
+++ script.js
@@ -0,0 +1,9 @@
+ let admin , name;
+ name = "Джон";
+ admin = name;
+ alert (admin);
+
+// let name = "Ilya" ;
+// alert ( `hello ${1} `) ; // hello 1
+// alert ( `hello ${"name"}`) ; // hello name
+// alert ( `hello ${name}`) ; // hello Ilya
